e109a882abe9f7b161763c38ae80ab79cf16a2d1
Milix-M <tujikou12dh@gmail.com>
2024-06-06 13:21:52 +0900

    add setTheme function

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,35 @@
-import { Totp } from './totp.ts'
+import { Totp } from "./totp.ts";
 
 const genTwoFaCode = () => {
   const totp = new Totp(6, 30);
   const key = totp.decodeB32Code("thisisasecretkey");
 
-  const code = totp.totp(key, new Date);
-  
+  const code = totp.totp(key, new Date());
+
   const twofacode = document.getElementById("twofacode");
 
   if (twofacode) {
     twofacode.innerHTML = code;
   }
-}
+};
+
+/**
+ * daisyuiのテーマを設定します.
+ */
+const setTheme = () => {
+  const htmltag = document.querySelector("html");
+  let theme = "dark";
+
+  // 環境変数にテーマが設定してあれば読み込む
+  if (import.meta.env.VITE_DAISYUI_THEME !== undefined) {
+    theme = import.meta.env.VITE_DAISYUI_THEME;
+  }
+
+  // htmlタグにテーマ設定
+  if (htmltag !== null) {
+    htmltag.setAttribute("data-theme", theme);
+  }
+};
 
 genTwoFaCode();
+setTheme();
