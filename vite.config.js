07f33f903c79e822670dd51dcf1d1a1a8e1d9d36
Milix-M <tujikou12dh@gmail.com>
2024-06-06 10:10:35 +0900

    add nodePolyfills() plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import manifest from "./manifest.json";
 
 export default defineConfig({
-  plugins: [crx({ manifest })],
-})
+  plugins: [crx({ manifest }), nodePolyfills()],
+});
