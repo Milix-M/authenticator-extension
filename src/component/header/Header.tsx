3cc385c98141fe2941921a6fd84407e32724c909
Milix-M <tujikou12dh@gmail.com>
2024-06-08 00:22:09 +0900

    update header design

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -6,15 +6,15 @@ const Header = () => {
     <div className="py-3 px-4 w-full flex items-center justify-between bg-base-100 border-b">
       <div className="flex space-x-2">
         {/* gear icon */}
-        <LuSettings className="w-5 h-5" />
+        <LuSettings className="w-5 h-5 hover:cursor-pointer" />
         <div className="w-5 h-5"></div>
       </div>
       <p className="text-center text-base font-bold">Authenticator</p>
       <div className="flex space-x-2">
         {/* Add icon */}
-        <FiPlusSquare className="w-5 h-5" />
+        <FiPlusSquare className="w-5 h-5 hover:cursor-pointer" />
         {/* QRCode icon */}
-        <LuQrCode className="w-5 h-5" />
+        <LuQrCode className="w-5 h-5 hover:cursor-pointer" />
       </div>
     </div>
   );
