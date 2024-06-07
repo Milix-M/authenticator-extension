a6544f1c8ab1514fc2b1314ca093577c6d8796d4
Milix-M <tujikou12dh@gmail.com>
2024-06-08 00:58:10 +0900

    show Modal system in header

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -2,6 +2,14 @@ import { FiPlusSquare } from "react-icons/fi";
 import { LuQrCode, LuSettings } from "react-icons/lu";
 
 const Header = () => {
+  const showAccModal = () => {
+    const modal = document.getElementById("addAccountModal") as HTMLDialogElement;
+
+    if (modal !== null) {
+      modal.showModal();
+    }
+  };
+
   return (
     <div className="py-3 px-4 w-full flex items-center justify-between bg-base-100 border-b">
       <div className="flex space-x-2">
@@ -12,7 +20,7 @@ const Header = () => {
       <p className="text-center text-base font-bold">Authenticator</p>
       <div className="flex space-x-2">
         {/* Add icon */}
-        <FiPlusSquare className="w-5 h-5 hover:cursor-pointer" />
+        <FiPlusSquare className="w-5 h-5 hover:cursor-pointer" onClick={() => showAccModal()}/>
         {/* QRCode icon */}
         <LuQrCode className="w-5 h-5 hover:cursor-pointer" />
       </div>
