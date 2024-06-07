fee1041790a51faefa0ba91b48e1e7f141e16396
Milix-M <tujikou12dh@gmail.com>
2024-06-07 23:08:20 +0900

    update header design

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,20 +1,20 @@
-import { BsQrCode } from "react-icons/bs";
-import { FaRegSquarePlus } from "react-icons/fa6";
-import { GoGear } from "react-icons/go";
+import { FiPlusSquare } from "react-icons/fi";
+import { LuQrCode, LuSettings } from "react-icons/lu";
 
 const Header = () => {
   return (
     <div className="py-3 px-4 w-full flex items-center justify-between bg-base-100 border-b">
-      <div>
+      <div className="flex space-x-2">
         {/* gear icon */}
-        <GoGear className="w-4 h-4" />
+        <LuSettings className="w-5 h-5" />
+        <div className="w-5 h-5"></div>
       </div>
-      <p className="text-center text-base">Authenticator</p>
+      <p className="text-center text-base font-bold">Authenticator</p>
       <div className="flex space-x-2">
         {/* Add icon */}
-        <FaRegSquarePlus className="w-4 h-4" />
+        <FiPlusSquare className="w-5 h-5" />
         {/* QRCode icon */}
-        <BsQrCode className="w-4 h-4" />
+        <LuQrCode className="w-5 h-5" />
       </div>
     </div>
   );
