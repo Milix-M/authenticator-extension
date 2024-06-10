07f9e7921ae7b6342359c30b7b9a16f0cbcabfd7
Milix-M <tujikou12dh@gmail.com>
2024-06-10 23:34:56 +0900

    utilsからAccountにgenTwofaCodeに関数を移動

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,3 +1,5 @@
+import { Totp } from "../totp";
+
 export class Account {
   constructor(
     public accountUUID: string,
@@ -7,4 +9,33 @@ export class Account {
     public counter?: number,
     public issuer?: string
   ) {}
+
+  /**
+   * 二段階認証コードをこのアカウントのsecret, type, counterに基づき生成します
+   * @returns 二段階認証コード
+   */
+  public genTwoFaCode = (): string => {
+    let code = "";
+
+    if (this.type === "totp") {
+      const totp = new Totp(6, 30);
+      const key = totp.decodeB32Code(this.secret);
+      code = totp.totp(key, new Date());
+    } else if (this.type === "hotp") {
+      const hotp = new Totp(6, 30);
+      const key = hotp.decodeB32Code(this.secret);
+
+      // counterが入ってなければデフォルトで0いれる
+      let generatorCounter = 0;
+
+      // 入ってればfgeneratorCounterにいれる
+      if (this.counter !== undefined) {
+        generatorCounter = this.counter;
+      }
+
+      code = hotp.hotp(key, generatorCounter);
+    }
+
+    return code;
+  };
 }
