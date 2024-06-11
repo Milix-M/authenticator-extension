dd0c3822b28172a3d13ae79d119fe8d0e0c854bb
Milix-M <tujikou12dh@gmail.com>
2024-06-11 17:03:56 +0900

    アカウント名編集inputをモーダルに追加

--- src/component/accountview/AccountView.tsx
+++ src/component/accountview/AccountView.tsx
@@ -85,6 +85,23 @@ const AccountView: React.FC<accountProps> = ({
           <h3 className="font-bold text-lg">アカウント編集</h3>
           <p className="break-words text-sm mt-2"></p>
 
+          <div className="mt-1 flex justify-center">
+              <label className="form-control w-full max-w-xs ">
+                <div className="label py-1">
+                  <span className="label-text">アカウント名</span>
+                  <span className="label-text-alt">必須</span>
+                </div>
+                <input
+                  id="account"
+                  type="text"
+                  placeholder="Account Name"
+                  className="input input-sm input-bordered w-full max-w-xs"
+                  onChange={() => {}}
+                  value={account.label}
+                />
+              </label>
+            </div>
+
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
