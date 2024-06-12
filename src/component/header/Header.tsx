e6f6e75041da94f67f006d86878fc0f435018d7e
Milix-M <tujikou12dh@gmail.com>
2024-06-12 10:13:10 +0900

    SettingsModalを表示できるように変更

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,7 +1,17 @@
+import { useRef } from "react";
 import { FiPlusSquare } from "react-icons/fi";
 import { LuQrCode, LuSettings } from "react-icons/lu";
+import SettingsModal from "../settingsmodal/SettingsModal";
 
 const Header = () => {
+  const settingsModalRef = useRef<HTMLDialogElement>(null);
+
+  const showSettingsModal = () => {
+    if (settingsModalRef.current !== null) {
+      settingsModalRef.current.showModal();
+    }
+  }
+
   const showAccModal = () => {
     const modal = document.getElementById("addAccountModal") as HTMLDialogElement;
 
@@ -11,20 +21,24 @@ const Header = () => {
   };
 
   return (
-    <div className="py-3 px-4 w-full flex items-center justify-between bg-base-100 border-b">
-      <div className="flex space-x-2">
-        {/* gear icon */}
-        <LuSettings className="w-5 h-5 hover:cursor-pointer" />
-        <div className="w-5 h-5"></div>
+    <>
+      <SettingsModal modalRef={settingsModalRef}/>
+
+      <div className="py-3 px-4 w-full flex items-center justify-between bg-base-100 border-b">
+        <div className="flex space-x-2">
+          {/* gear icon */}
+          <LuSettings className="w-5 h-5 hover:cursor-pointer" onClick={() => showSettingsModal()}/>
+          <div className="w-5 h-5"></div>
+        </div>
+        <p className="text-center text-base font-bold">Authenticator</p>
+        <div className="flex space-x-2">
+          {/* Add icon */}
+          <FiPlusSquare className="w-5 h-5 hover:cursor-pointer" onClick={() => showAccModal()}/>
+          {/* QRCode icon */}
+          <LuQrCode className="w-5 h-5 hover:cursor-pointer" />
+        </div>
       </div>
-      <p className="text-center text-base font-bold">Authenticator</p>
-      <div className="flex space-x-2">
-        {/* Add icon */}
-        <FiPlusSquare className="w-5 h-5 hover:cursor-pointer" onClick={() => showAccModal()}/>
-        {/* QRCode icon */}
-        <LuQrCode className="w-5 h-5 hover:cursor-pointer" />
-      </div>
-    </div>
+    </>
   );
 };
 
