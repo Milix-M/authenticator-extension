d174396fc614359a212a6402684253a80dd42644
Milix-M <tujikou12dh@gmail.com>
2024-06-12 18:12:37 +0900

    update テーマ設定メソッド

--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,15 @@
 /**
  * daisyuiのテーマを設定します.
  */
-const setTheme = () => {
-    const htmltag = document.querySelector("html");
-    let theme = "dark";
-  
-    // 環境変数にテーマが設定してあれば読み込む
-    if (import.meta.env.VITE_DAISYUI_THEME !== undefined) {
-      theme = import.meta.env.VITE_DAISYUI_THEME;
-    }
-  
-    // htmlタグにテーマ設定
-    if (htmltag !== null) {
+export const setThemeToDaisyui = (theme: string | null) => {
+  const htmltag = document.querySelector("html");
+
+  // htmlタグにテーマ設定
+  if (htmltag !== null) {
+    if (theme !== null) {
       htmltag.setAttribute("data-theme", theme);
+    } else {
+      htmltag.setAttribute("data-theme", "light");
     }
-  };
-  
-setTheme();
+  }
+};
