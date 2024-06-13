66e777c94783f47a471c0815d89bb3e4839e9357
Milix-M <tujikou12dh@gmail.com>
2024-06-13 22:31:22 +0900

    readQRtoAccountを実装

--- src/qrcodereader.ts
+++ src/qrcodereader.ts
@@ -1,14 +1,43 @@
+import jsQR from "jsqr";
+import { PNG } from "pngjs";
+import parseURI from "otpauth-uri-parser";
+import { Account } from "./models/account";
+import { v4 as uuidv4 } from "uuid";
+
 export async function readQRtoAccount() {
   // タブ情報取得
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   if (tab.url !== undefined && !/chrome:\/\//.test(tab.url)) {
-    chrome.tabs.captureVisibleTab(tab.windowId, { format: 'png' }, (result) => {
-        const child = document.createElement('img');
-        child.setAttribute('src', result)
+    chrome.tabs.captureVisibleTab(tab.windowId, { format: "png" }, (result) => {
+      const png = PNG.sync.read(
+        Buffer.from(result.slice("data:image/png;base64,".length), "base64")
+      );
+      const code = jsQR(
+        Uint8ClampedArray.from(png.data),
+        png.width,
+        png.height
+      );
 
-        document.body.appendChild(child)
-        console.log("readQR!")
+      if (code !== null) {
+        const parsed = parseURI(code.data);
+
+        // Accountクラスに戻す
+        const account: Account = Object.assign(
+          new Account(
+            uuidv4(),
+            parsed.query.secret,
+            parsed.type,
+            parsed.label.issuer + parsed.label.account,
+            new Date().getTime(),
+            parsed.query.period,
+            parsed.query.counter,
+            parsed.query.issuer
+          )
+        );
+
+        return account;
+      }
     });
   }
 }
