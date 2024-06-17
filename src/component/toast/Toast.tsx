5b371d7ccb47e54bd99920aa35e02084e962fe02
Milix-M <tujikou12dh@gmail.com>
2024-06-17 17:20:41 +0900

    クラス名変更

--- src/component/toast/Toast.tsx
+++ src/component/toast/Toast.tsx
@@ -7,7 +7,7 @@ const Toast: React.FC<toastProps> = ({ toastText, toastType }) => {
   return (
     <>
       <div className="toast toast-center toast-middle z-[100] select-none">
-        <div className={`alert alert-${toastType}`}>
+        <div className={`alert ${toastType}`}>
           <span className="text-sm">{toastText}</span>
         </div>
       </div>
