303180dcc1763b99d9d7f519f7900663eb622c5d
Milix-M <tujikou12dh@gmail.com>
2024-06-18 13:47:27 +0900

    アカウントインポートを実装

--- src/backup.tsx
+++ src/backup.tsx
@@ -1,4 +1,6 @@
+import { Account } from "./models/account";
 import { StorageProvider } from "./storage";
+import { v4 as uuidv4 } from "uuid";
 
 /**
  * アカウントをエクスポートしてダウンロードさせます.
@@ -14,6 +16,18 @@ export const exportAccounts = () => {
   });
 };
 
-export const importAccounts = () => {
+export const importAccounts = (e: File) => {
+  const reader = new FileReader();
+  const storageProvider = new StorageProvider();
 
-}
+  reader.onloadend = async () => {
+    const parsedJson = JSON.parse(reader.result as string);
+    for (const [_, v] of Object.entries(parsedJson)) {
+      await storageProvider.setSecret(
+        Object.assign(new Account(uuidv4(), "", "totp", "", 0), v)
+      );
+    }
+  };
+
+  reader.readAsText(e);
+};
