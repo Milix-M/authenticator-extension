2b489a780cb8c7aac464ba3f1950a93eacd15a6b
Milix-M <tujikou12dh@gmail.com>
2024-06-29 03:00:41 +0900

    onMessageにイベントリスナを追加, contextMenuの作成, 削除関数を追加

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
+import { Account } from "./models/account";
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "insertTwoFactorCodeMenu",
@@ -5,3 +7,24 @@ chrome.runtime.onInstalled.addListener(() => {
     contexts: ["editable"],
   });
 });
+
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.addAccount) {
+    insertAccountMenuToParent(request.addAccount);
+  } else if (request.deleteAccount) {
+    removeAccountMenu(request.deleteAccount);
+  }
+});
+
+function insertAccountMenuToParent(account: Account) {
+  chrome.contextMenus.create({
+    id: account.accountUUID,
+    parentId: "insertTwoFactorCodeMenu",
+    title: account.label,
+    contexts: ["editable"],
+  });
+}
+
+function removeAccountMenu(accountUUID: string) {
+  chrome.contextMenus.remove(accountUUID);
+}
