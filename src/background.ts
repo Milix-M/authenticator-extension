53e34da51aef13d22ac91e441816d5d6a6d48e01
Milix-M <tujikou12dh@gmail.com>
2024-07-01 17:40:21 +0900

    右クリックされた際に二段階認証コードを挿入できるように変更

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 import { Account } from "./models/account";
+import { StorageProvider } from "./storage";
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -14,6 +15,39 @@ chrome.runtime.onMessage.addListener((request) => {
   } else if (request.deleteAccount) {
     removeAccountMenu(request.deleteAccount);
   }
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const storageProvider = new StorageProvider();
+
+  storageProvider.getSecrets().then((values) => {
+    let twofaCode: string | undefined;
+
+    //該当するアカウント取ってくる
+    for (const value of values) {
+      if (value.accountUUID === info.menuItemId) {
+        twofaCode = value.genTwoFaCode();
+      }
+    }
+
+    if (tab !== undefined && tab.id !== undefined) {
+      // chrome内蔵のページで動作させるとエラー発生する
+      if (tab.url?.startsWith("chrome://")) return;
+
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: (twofaCode) => {
+          const activeEl = document.activeElement as HTMLInputElement;
+
+          // アクティブな要素アリ & acc 存在確認
+          if (activeEl !== null && twofaCode !== undefined) {
+            activeEl.value = twofaCode;
+          }
+        },
+        args: [twofaCode],
+      });
+    }
+  });
 });
 
 function insertAccountMenuToParent(account: Account) {
