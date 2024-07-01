20525c49221fc7c7106fe229a9e6b0cdbde46e0d
Milix-M <tujikou12dh@gmail.com>
2024-07-02 00:21:38 +0900

    WIP コンテキストメニューを追加

---
+++ src/background.ts
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "injectTwoFaCode",
+    title: "二段階認証コードを挿入...",
+    contexts: ["editable"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "injectTwoFaCode":
+      chrome.windows.create({
+        url: "../index.html",
+        type: "popup",
+        width: 350,
+        height: 500,
+        focused: true,
+      });
+      console.log(tab);
+  }
+});
