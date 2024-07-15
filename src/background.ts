bf1bf08a32c9a4bc09074adb80aa5fb78c25d990
Milix-M <tujikou12dh@gmail.com>
2024-07-15 22:22:46 +0900

    アクティブだったinputエレメントにuuidを追加してクエリとしてポップアップにtabidとuuidを渡せるように変更

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "injectTwoFaCode",
@@ -7,15 +9,35 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  switch (info.menuItemId) {
-    case "injectTwoFaCode":
-      chrome.windows.create({
-        url: "../popup.html",
-        type: "popup",
-        width: 340,
-        height: 520,
-        focused: true,
-      });
-      console.log(tab);
+  const classNameUUID = uuidv4();
+
+  if (tab !== undefined && tab.id !== undefined) {
+    // chrome内蔵のページで動作させるとエラー発生する
+    if (tab.url?.startsWith("chrome://")) return;
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: (classNameUUID) => {
+        const activeEl = document.activeElement as HTMLInputElement;
+
+        if (activeEl !== null) {
+          // ポップアップ側でアクティブだった要素を特定するためにuuidをクラス名に追加する
+          activeEl.classList.add(classNameUUID);
+        }
+      },
+      args: [classNameUUID],
+    });
+
+    switch (info.menuItemId) {
+      case "injectTwoFaCode":
+        chrome.windows.create({
+          url: `../popup.html?uuid=${classNameUUID}&tabid=${tab?.id}`,
+          type: "popup",
+          width: 340,
+          height: 520,
+          focused: true,
+        });
+        console.log(tab);
+    }
   }
 });
