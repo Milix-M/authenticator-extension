0309bb9021f01d8476de9ca394176dacd2e2314b
Milix-M <tujikou12dh@gmail.com>
2025-03-06 10:34:51 +0900

    言語検知と非同期翻訳ファイル読み込みを出来るように設定変更

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,27 +1,29 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-// 言語jsonファイルのimport
-import translation_en from "./en.json";
-import translation_ja from "./ja.json";
+// サポートする言語
+export const supportedLngs = {
+  en: "English",
+  ja: "日本語",
+};
 
-const resources = {
-    ja: {
-      translation: translation_ja
-    },
-    en: {
-      translation: translation_en
-    }
-  };
-
-  i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+i18n
+  .use(HttpApi) // 翻訳ファイルを非同期に読み込むようにする
+  .use(LanguageDetector) // ユーザーの言語設定を検知するようにする
+  .use(initReactI18next) // i18next インスタンスを初期化
   .init({
-    resources,
-    lng: "ja", 
+    fallbackLng: "ja", // フォールバック言語。指定された言語ファイルがない場合などにこの言語が使用される
+    returnEmptyString: false, // 空文字での定義を許可する
+    supportedLngs: Object.keys(supportedLngs),
+
+    // デフォルトは`escapeValue: true`
+    // 18next が翻訳メッセージ内のコードをエスケープし、XSS 攻撃から保護するためのもの
+    // React がこのエスケープを行ってくれるので、今回はこれをオフにする
     interpolation: {
-      escapeValue: false // react already safes from xss
-    }
+      escapeValue: false,
+    },
   });
 
-  export default i18n;
+export default i18n;
