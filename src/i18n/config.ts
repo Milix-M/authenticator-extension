b34a12df53fe955d0c261038487a01709d00e3dd
Milix-M <tujikou12dh@gmail.com>
2025-03-18 16:34:37 +0900

    言語ファイルの読み込み方を変更

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
+import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 // サポートする言語
@@ -10,13 +10,16 @@ export const supportedLngs = {
 };
 
 i18n
-  .use(HttpApi) // 翻訳ファイルを非同期に読み込むようにする
+  .use(Backend) // 翻訳ファイルを非同期に読み込むようにする
   .use(LanguageDetector) // ユーザーの言語設定を検知するようにする
   .use(initReactI18next) // i18next インスタンスを初期化
   .init({
     fallbackLng: "ja", // フォールバック言語。指定された言語ファイルがない場合などにこの言語が使用される
     returnEmptyString: false, // 空文字での定義を許可する
     supportedLngs: Object.keys(supportedLngs),
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
 
     // デフォルトは`escapeValue: true`
     // 18next が翻訳メッセージ内のコードをエスケープし、XSS 攻撃から保護するための設定
