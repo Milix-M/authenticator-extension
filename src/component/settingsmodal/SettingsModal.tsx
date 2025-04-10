8c4d39413685d010d630f7c08cdc69b52ecfb189
Milix-M <tujikou12dh@gmail.com>
2025-04-10 17:55:28 +0900

    useTranslationが消えていたのを修正

--- src/component/settingsmodal/SettingsModal.tsx
+++ src/component/settingsmodal/SettingsModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { useTranslation } from "react-i18next";
 import { supportedTheme } from "../../supportedTheme";
 import { setThemeToDaisyui } from "../../theme";
 import { exportAccounts } from "../../backup";
